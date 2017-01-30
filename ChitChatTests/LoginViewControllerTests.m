eca144dc6f3b993b308c4e2f18afd1802e44184f
Yeradis P. Barbosa Marrero <yeradis@gmail.com>
2017-01-30 23:06:33 +0100

    Add some tests

--- ChitChatTests/LoginViewControllerTests.m
+++ ChitChatTests/LoginViewControllerTests.m
@@ -1,9 +1,16 @@
 #import <XCTest/XCTest.h>
 #import "LoginViewController.h"
 #import "ChitChat.h"
+#import <OCMock/OCMock.h>
+
+@interface LoginViewController (Testing)
+-(void) showMessages;
+-(void) setLoginErrorStateWithMessage:(NSString*) msg;
+@end
 
 @interface LoginViewControllerTests : XCTestCase
 @property LoginViewController* vc;
+@property id partiallyMockedApi;
 @end
 
 @implementation LoginViewControllerTests
@@ -27,6 +34,43 @@
 
 -(void) test_viewLoad_ShouldNoThrow {
     XCTAssertNoThrow(self.vc.view);
+}
+
+-(void) test_doLogin_HavingAValidUserName_ShouldCallshowMessages {
+    ChitChat* api = [[ChitChat alloc] init];
+    self.vc.serviceClient = api;
+    self.partiallyMockedApi = [OCMockObject partialMockForObject:self.vc];
+    [[self.partiallyMockedApi expect] showMessages];
+
+    XCTAssertNoThrow([self.partiallyMockedApi view]);
+    [self.partiallyMockedApi loginInput].text = @"user name";
+    [self.partiallyMockedApi doLogin:nil];
+    [self.partiallyMockedApi verify];
+}
+
+-(void) test_doLogin_HavingAnInvalidValidUserName_ShouldCallSetsetLoginErrorStateWithMessage {
+    ChitChat* api = [[ChitChat alloc] init];
+    self.vc.serviceClient = api;
+    self.partiallyMockedApi = [OCMockObject partialMockForObject:self.vc];
+    [[self.partiallyMockedApi expect] setLoginErrorStateWithMessage:OCMOCK_ANY];
+    
+    XCTAssertNoThrow([self.partiallyMockedApi view]);
+    [self.partiallyMockedApi loginInput].text = @"";
+    [self.partiallyMockedApi doLogin:nil];
+    [self.partiallyMockedApi verify];
+}
+
+-(void) test_doLogin_HavingAReservedUserName_ShouldCallSetsetLoginErrorStateWithMessage {
+    ChitChat* api = [[ChitChat alloc] init];
+    self.vc.serviceClient = api;
+    self.partiallyMockedApi = [OCMockObject partialMockForObject:self.vc];
+    [[self.partiallyMockedApi expect] setLoginErrorStateWithMessage:OCMOCK_ANY];
+    
+    XCTAssertNoThrow([self.partiallyMockedApi view]);
+    [self.partiallyMockedApi loginInput].text = @"Lily";
+    [self.partiallyMockedApi doLogin:nil];
+    
+    [self.partiallyMockedApi verify];
 }
 
 @end
